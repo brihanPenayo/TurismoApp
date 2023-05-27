527d4012251de5c33d3cefaf682f244e90413b8a
Marcos Ferreira <ferreiramarcosadrian@gmail.com>
2023-05-27 11:28:17 -0400

    Se agg componentes

---
+++ src/pages/FormChofer.jsx
@@ -0,0 +1,14 @@
+import Header from '../partials/Header'
+import Footer from '../partials/Footer'
+
+const FormChofer = () => {
+	return (
+		<>
+			<Header />
+
+			<Footer />
+		</>
+	)
+}
+
+export default FormChofer
